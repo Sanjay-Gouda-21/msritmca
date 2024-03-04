779726ee01003f400e992cb6436d1cb3dc0a6bf4
Sanjay-Gouda-21 <sanjaygouda759@gmail.com>
2024-03-04 14:36:03 +0530

    altered

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -2,18 +2,14 @@ import React from "react";
 
 function Home() {
   return (
-    <section className="bg-gray-50">
+    <section className="bg-white">
       <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
         <div className="mx-auto max-w-xl text-center">
-          <h1 className="text-3xl font-extrabold sm:text-5xl">
-            Welcome to MCAPEDIA
-            {/* <strong className="font-bold size-25 mt-5 text-primary sm:block">
-              {" "}
-              Department of MCA{" "}
-            </strong> */}
+          <h1 className="text-3xl font-extrabold sm:text-5xl text-red-800">
+            Welcome to <span className="text-primary"> MCAPEDIA</span>
           </h1>
 
-          <p className="mt-4 sm:text-xl/relaxed">
+          <p className="mt-4 sm:text-xl/relaxed text-blue-800">
             Your hub for accessing notes, question papers, and other resources.
           </p>
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 // page.tsx
 import React from "react";
 import Layout from "./layout";
+import Hero from "./components/Hero";
 
 const Page: React.FC = () => {
-  return <></>;
+  return <Hero />;
 };
 
 export default Page;
