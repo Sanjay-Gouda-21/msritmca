76e5598704b2d9036f71d7f4191f5247f46fa59a
Sanjay-Gouda-21 <sanjaygouda759@gmail.com>
2024-03-04 14:30:04 +0530

    created

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -2,14 +2,18 @@ import React from "react";
 
 function Home() {
   return (
-    <section className="bg-white">
+    <section className="bg-gray-50">
       <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
         <div className="mx-auto max-w-xl text-center">
-          <h1 className="text-3xl font-extrabold sm:text-5xl text-red-800">
-            Welcome to <span className="text-primary"> MCAPEDIA</span>
+          <h1 className="text-3xl font-extrabold sm:text-5xl">
+            Welcome to MCAPEDIA
+            {/* <strong className="font-bold size-25 mt-5 text-primary sm:block">
+              {" "}
+              Department of MCA{" "}
+            </strong> */}
           </h1>
 
-          <p className="mt-4 sm:text-xl/relaxed text-blue-800">
+          <p className="mt-4 sm:text-xl/relaxed">
             Your hub for accessing notes, question papers, and other resources.
           </p>
 
